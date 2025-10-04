d466acbf5fcae7e6979e2710732b2e0f78510b69
Aaron Franks <aaron.franks@gmail.com>
2025-10-04 15:18:34 -0700

    Proof-of-content markdown comment docs

--- playground/server.ts
+++ playground/server.ts
@@ -2,6 +2,27 @@
 // See https://bun.sh/docs/bundler/fullstack
 import app from './index.html'
 
+import fs from 'node:fs'
+import { marked } from 'marked'
+
+const commentDocRegex = /^\/\*\*(.+)?\*\*\//s
+const contentToRoute = (fname: string) => () => {
+  const content = fs.readFileSync(`./src/${fname}`, 'utf-8')
+  const commentMatch = commentDocRegex.exec(content)
+  const markdown = commentMatch?.[1].replace(/^\s*\*\s?/gm, '').trim()
+
+  return new Response(`<!DOCTYPE html><html>${markdown ? marked(markdown) : 'No comment'}</html>`, {
+    headers: { 'Content-Type': 'text/html' },
+  })
+}
+
+const fileTuples = fs
+  .readdirSync('./src')
+  .filter((fname) => fname.endsWith('.css'))
+  .map((fname) => ['/' + fname.replace('.css', ''), contentToRoute(fname)])
+
+const routes = Object.fromEntries(fileTuples)
+
 const port = 3333
 
 Bun.serve({
@@ -9,6 +30,7 @@ Bun.serve({
   development: true, // Enables hot reloading
   routes: {
     '/*': app,
+    ...routes,
   },
 })
 
